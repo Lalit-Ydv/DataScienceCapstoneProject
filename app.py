b694cca69b01c232a5e7ffb1f4bb36435f418a30
Lalit-Ydv <132750176+Lalit-Ydv@users.noreply.github.com>
2023-08-13 07:41:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,62 @@
 import streamlit as st
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 import pickle
-with open('C:/Users/dell/Downloads/app.py', 'rb') as file:
-    python_code = file.read()
 
-pickled_code = pickle.dumps(python_code)
+# Function to create a simple ML pipeline and perform predictions
+def predict_prices(car_detalis):
+    # Load the saved pipeline
+    car_details['manufacturer']=car_details['name'].apply(lambda x:x.split(' ')[0])
+    car_details['model']=car_details['name'].apply(lambda x:x.split(' ')[1])
+    car_details['No_of_total_years']=2023-car_details['year']
+    car_details.drop(['name','year'],axis=1,inplace=True) 
+    with open(r'pipeline.pkl', 'rb') as file:
+        pipeline = pickle.load(file)
 
-with open('C:/Users/dell/Downloads/app'.pkl, 'wb') as file:
-    file.write(pickled_code)
+
+    # Make predictions
+    predictions = pipeline.predict(car_details)
+
+    car_details['predicted_selling_price'] = predictions
+    return car_details['predicted_selling_price']
 
 def main():
-    st.title('Car Price Prediction Using ML')
-    st.subheader('Car Price Predictor')
-    st.info('''We need some information to predict Car price''')
+    st.title("Car Details Input and Prediction")
+    st.header("Enter Car Details")
 
+    # Input fields for each column
+    name = st.text_input("Name")
+    year = st.number_input("Year", min_value=1980, max_value=2023, step=1)
+    km_driven = st.number_input("Kilometers Driven", min_value=0, step=1000)
+    fuel = st.selectbox("Fuel Type", ["Petrol", "Diesel", "CNG", "LPG", "Electric"])
+    seller_type = st.selectbox("Seller Type", ["Individual", "Dealer", "Trustmark Dealer"])
+    transmission = st.selectbox("Transmission", ["Manual", "Automatic"])
+    owner = st.selectbox("Owner", ["First Owner", "Second Owner", "Third Owner", "Fourth & Above Owner"])
 
-    car_details = pd.read_csv('C:/Users/dell/Desktop/capstone/CAR DETAILS.csv')
-    Brand=(car_details['name'].unique())
-    transmission=(car_details['transmission'].unique())
-    seller=(car_details['seller_type'].unique())
-    owner=(car_details['owner'].unique())
-    fuel=(car_details['fuel'].unique())
+    # Display the input data in a DataFrame
+    data = {
+        "name": name,
+        "year": year,
+        "km_driven": km_driven,
+        "fuel": fuel,
+        "seller_type": seller_type,
+        "transmission": transmission,
+        "owner": owner
+    }
+    car_details = pd.DataFrame(data,index=[0])
 
-    p2=st.slider('Model Year',2005,2020,2005)
+    # Prediction button
+    if st.button("Predict"):
+        # Call the predict_prices function to perform predictions
+        df_with_predictions = predict_prices(df)
 
-    p3=st.selectbox('Seller Type',seller)
-    if p3=='Individual':
-        p3=1
-    elif p3=='Dealer':
-        p3=0
-    elif p3=='Trustmark Dealer':
-        p3=2
+        # Display the DataFrame with predicted selling prices
+        st.header("Car Details with Predicted Selling Prices")
+        st.dataframe(df_with_predictions)
 
-    p4=st.selectbox('Owner Type',owner)
-    if p4=='First Owner':
-        p4=0
-    elif p4=='Second Owner':
-        p4=2
-    elif p4=='Third Owner':
-        p4=4
-    elif p4=='Fourth & Above Owner':
-        p4=1
-    elif p4=='Test Drive Car':
-        p4=3
+if __name__ == "__main__":
+    # Train the model and save the pipeline (you can do this in a separate script)
+    #train_and_save_pipeline()
 
-    p5=st.selectbox('Transmission Type',transmission)
-    if p5=='Manual':
-        p5=1
-    elif p5=='Automatic':
-        p5=0
-
-    p6=st.selectbox('Fuel Type',fuel)
-    if p6=='Petrol':
-        p6=4
-    elif p6=='Diesel':
-        p6=1
-    elif p6=='CNG':
-        p6=0
-    elif p6=='LPG':
-        p6=3
-    elif p6=='Electric':
-        p6=2
-
-    p7=(st.slider('KM Driven',500,10000000,500))/100000
-
-    x=pd.DataFrame({'year':[p2],'fuel':[p6],'seller_type':[p3],
-                    'transmission':[p5],'owner':[p4],'km_driven_in_lacks':[p7]})
-    ok=st.button('Predict Car Price')
-    if ok:
-        prediction=model.predict(x)
-        st.success('Predicted Car Price:'+str( prediction*100000) +'Rupees')
-        st.caption('Thanks for using!')
-        st.balloons()
-        st.write('Created by Lalit Yadav.')
-
-if __name__=='__main__':
+    # Run the Streamlit app
     main()
